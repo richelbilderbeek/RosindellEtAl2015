aae602929788198e15b5301b368531b61dbd5f12
richelbilderbeek <richel@richelbilderbeek.nl>
2019-12-28 10:57:58 +0100

    Use Urho3D folder next to root folder

--- Refactored/mastercontrol.cpp
+++ Refactored/mastercontrol.cpp
@@ -58,11 +58,11 @@ MasterControl::MasterControl(Context *context):
     Application(context)
 {
   {
-    const int error{std::system("ln -s ../Urho3D/bin/Data")};
+    const int error{std::system("ln -s ../../Urho3D/bin/Data")};
     if (error) {}
   }
   {
-    const int error{std::system("ln -s ../Urho3D/bin/CoreData")};
+    const int error{std::system("ln -s ../../Urho3D/bin/CoreData")};
     if (error) {}
   }
 }
